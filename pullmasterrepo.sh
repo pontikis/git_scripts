2c6bcbc9da74dc29a209c9791b294691e76ea0e5
Christos Pontikis <pontikis@gmail.com>
2016-08-02 18:36:42 +0300

    pullmasterrepo.sh

---
+++ pullmasterrepo.sh
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# SCRIPT.........: pullmasterrepo.sh
+# ACTION.........: Pull master branch from Github to a local repo
+# COPYRIGHT......: Christos Pontikis (http://www.pontikis.gr)
+# LICENSE........: MIT (https://opensource.org/licenses/MIT)
+#-------------------------------------------------------------------------------
+
+#!/bin/bash
+
+echo '##################################################'
+
+echo -e '\ngit status .......................................'
+git status
+
+echo -e '\nremote repositories ..............................'
+git remote -v
+
+echo -e '\nbranches .........................................'
+git branch -a
+
+echo -e '\ncurrent master version ..............................'
+echo "local  `git describe master`"
+echo "github `git describe github/master`"
+
+echo -e '\ncompare local and github master .....................'
+git diff --stat --color dev github/dev
+
+echo -e '\n--------------------------------------------------'
+
+read -p "Pull master from github to repo $(pwd) ? (1=yes otherwhise exit): " opt
+if [ "$opt" != "1" ]; then exit; fi
+
+set -xe
+
+git fetch
+git checkout master
+git merge github/master
